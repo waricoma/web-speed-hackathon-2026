30d7e11aac8fbc7e7e9712a36621509c9e82c4dd
waricoma <motone.adachi@nightlamp.co.jp>
2026-03-21 16:24:44 +0900

    フロント改善: パフォーマンス罠除去(262144配列、1msポーリング、500ms遅延、1ms再計算)

--- application/client/src/components/foundation/InfiniteScroll.tsx
+++ application/client/src/components/foundation/InfiniteScroll.tsx
@@ -13,14 +13,9 @@ export const InfiniteScroll = ({ children, fetchMore, items }: Props) => {
 
   useEffect(() => {
     const handler = () => {
-      // 念の為 2の18乗 回、最下部かどうかを確認する
-      const hasReached = Array.from(Array(2 ** 18), () => {
-        return window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
-      }).every(Boolean);
+      const hasReached = window.innerHeight + Math.ceil(window.scrollY) >= document.body.offsetHeight;
 
-      // 画面最下部にスクロールしたタイミングで、登録したハンドラを呼び出す
       if (hasReached && !prevReachedRef.current) {
-        // アイテムがないときは追加で読み込まない
         if (latestItem !== undefined) {
           fetchMore();
         }
@@ -29,19 +24,14 @@ export const InfiniteScroll = ({ children, fetchMore, items }: Props) => {
       prevReachedRef.current = hasReached;
     };
 
-    // 最初は実行されないので手動で呼び出す
     prevReachedRef.current = false;
     handler();
 
-    document.addEventListener("wheel", handler, { passive: false });
-    document.addEventListener("touchmove", handler, { passive: false });
-    document.addEventListener("resize", handler, { passive: false });
-    document.addEventListener("scroll", handler, { passive: false });
+    window.addEventListener("scroll", handler, { passive: true });
+    window.addEventListener("resize", handler, { passive: true });
     return () => {
-      document.removeEventListener("wheel", handler);
-      document.removeEventListener("touchmove", handler);
-      document.removeEventListener("resize", handler);
-      document.removeEventListener("scroll", handler);
+      window.removeEventListener("scroll", handler);
+      window.removeEventListener("resize", handler);
     };
   }, [latestItem, fetchMore]);
 
